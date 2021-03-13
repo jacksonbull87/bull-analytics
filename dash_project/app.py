f96aaf3bde50f103cfe02c365f78f948b88220f5
Jackson <jacksonbull1987@gmail.com>
2021-03-13 18:55:11 -0500

    primary code for dash application

--- dash_project/app.py
+++ dash_project/app.py
@@ -2,6 +2,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import numpy as np
+from dash.dependencies import Output, Input
+
 
 data = pd.read_csv("one_month_trax.csv")
 # data = data.query("type == 'conventional' and region == 'Albany'")
@@ -9,39 +12,86 @@ data = pd.read_csv("one_month_trax.csv")
 data.sort_values("total_ig_followers", ascending=False ,inplace=True)
 
 app = dash.Dash(__name__)
+app.title = "Artist Discovery Tool: MVP"
+
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Artist Discovery: MVP",),
+        html.H1(
+            children="Artist Discovery: MVP",
+            style={"fontsize": "48px", "color": "green", 'text-align': 'center'}
+        ),
         html.P(
-            children="Analyze Weekly Tiktok Data",
+            children="This insights tools is designed to analyze Tiktok's Top 100 Weekly chart by identifying the artists with the biggest fanbase on Instagram prior to the record's first occurance on the chart.",
+            style={"fontsize": "15px", "color": "black", 'text-align': 'center'}
         ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["artist"],
-                        "y": data["total_ig_followers"],
-                        "type": "bar",
-                    },
-                ],
-                "layout": {"title": "Top Tiktok Artists"},
-            },
+        html.P(
+            children="From the perspective of talent seekers and A&R professionals, one of the inherent risks when pouring more resources into a client is the possibility that they'll be no one on the other side to receive the creator's message. This tool mitigates that risk by ensuring that there is an existing audience to build off of",
+            style={"fontsize": "15px", "color": "black", 'text-align': 'center'}),
+
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Career Stage"),
+                        dcc.Dropdown(
+                            id="career-filter",
+                            options=[
+                                {"label": stage, "value": stage}
+                                for stage in np.sort(data['Career Stage'].unique())
+                            ],
+                            # value="Indie",
+                            clearable=False,
+                            # className="dropdown",
+                        ),
+                    ]
+                ),
+            ]
         ),
-        # dcc.Graph(
-        #     figure={
-        #         "data": [
-        #             {
-        #                 "x": data["Artists"],
-        #                 "y": data["Total IG Followers"],
-        #                 "type": "bar",
-        #             },
-        #         ],
-        #         "layout": {"title": "Avocados Sold"},
-        #     },
-        # ),
+     
+        html.Div(
+            children=dcc.Graph(
+                id="ig-chart", config={"displayModeBar": False},
+            )
+
+        ),
     ]
 )
+@app.callback(
+    Output("ig-chart", "figure"),
+    
+        [Input("career-filter", "value")],
+        # Input("type-filter", "value"),
+        # Input("date-range", "start_date"),
+        # Input("date-range", "end_date"),
+)
+        
+def update_charts(stage):
+    mask = data['Career Stage'] == stage
+
+    
+    filtered_data = data.loc[mask, :]
+    ig_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["artist"],
+                "y": filtered_data["total_ig_followers"],
+                "type": "bar",
+                # "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Top Tiktok Artists by Instagram Fanbase",
+                # "x": 0.05,
+                # "xanchor": "left",
+            }
+            # "xaxis": {"fixedrange": True},
+            # "yaxis": {"tickprefix": "$", "fixedrange": True},
+            # "colorway": ["#17B897"],
+        }
+    }
+    return ig_chart_figure
 
 
 
