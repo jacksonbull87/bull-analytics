3ebec0883b49348bc022d466eee78c632ebcd201
Jackson <jacksonbull1987@gmail.com>
2021-03-18 20:11:34 -0400

    removed unnecessary files

--- dash_project/blog3/cm_config.py
+++
@@ -1,5 +0,0 @@
-config = {'refresh_token' : 'rxIaN6wlwAhTMiC8WrJv4wUpDmN1xuAn01yhU4hh1FMb3CKOo9fv9RDpjeK8tMsM',
-'consumer_key': 'xMJuhJlndH1Y89eXX1vY8S0uO',
-'consumer_secret_key': 'USIPZc4VJV8RDjsXkNo9PpIrdGKyHFTyK8tBnwl8o0QgkFMvJB', 
-'access_token': '63628835-MEG8Sfprjp3DJeXLjJ7vZEGrEJaOqZm30mpZPdAAf', 
-'access_token_secret': '9SuGHDMmKUykKBadjLtDrO3hRPt2m1j2jOJFgBUfn48q4'}
